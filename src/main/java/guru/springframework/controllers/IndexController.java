ea15d21b44826fc36b978238d3154c262530b694
Wojciech <w.wasilewski@gmail.com>
2018-08-27 18:19:53 +0200

    adding methods in repositories + improving index.html

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,15 +1,34 @@
 package guru.springframework.controllers;
 
+import guru.springframework.domain.Category;
+import guru.springframework.domain.UnitOfMeasure;
+import guru.springframework.repositories.CategoryRepository;
+import guru.springframework.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController
 {
+    private CategoryRepository catRepo;
+    private UnitOfMeasureRepository unitOfMeasureRepo;
+
+    public IndexController(CategoryRepository categRepo, UnitOfMeasureRepository unitOfMeasureRepo) {
+        this.catRepo = categRepo;
+        this.unitOfMeasureRepo = unitOfMeasureRepo;
+    }
+
     @RequestMapping({"","/","/index"})
     public String getIndex()
     {
+        Optional<Category> categoryOptional = catRepo.findByDescription("Mexican");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepo.findByDescription("Teaspoon");
+
+        System.out.println("Cat id is: " + categoryOptional.get().getId());
+        System.out.println("UOM id is: " + unitOfMeasureOptional.get().getId());
+
         return "index";
     }
 }
-
